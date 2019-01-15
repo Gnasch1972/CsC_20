2cf66bddd8e60d89d21624b685522321ea81657f
Gnasch1972 <juiblex_ii@yahoo.com>
2019-01-15 13:56:44 -0800

    files added after completeting practices

---
+++ CONTRIBUTING.md
@@ -0,0 +1,4 @@
+Project:  CsC20 of The Great Work
+
+Contributors:  Joseph Cunningham (owner)
+               Maybe others too.
---
+++ Chapter_2/Fig2_3/Fig2_3.c
@@ -0,0 +1,12 @@
+/* Figure 2.3: Fig2_3.c
+   Printing on one line with two printf statements */
+
+#include<stdio.h>
+
+int main(void)
+{
+  printf("Welcome ");
+  printf("to C!\n");
+
+  return 0;
+}
